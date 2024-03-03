8d797cf75bd2537e42dda4b818deca72a307b7e5
wedo <costadefelipe@gmail.com>
2024-03-03 13:22:45 -0300

    configurado o projeto

---
+++ src/main/java/com/challenge/cubo/CuboApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.cubo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/cubo/CuboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.cubo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
